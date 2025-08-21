46fa628bb96943eafa0d236132029ae7fef43360
rene <deshan-xiong@outlook.com>
2025-08-21 13:46:50 +0800

    feat: generic minheap

--- scheduler/minHeap.js
+++ scheduler/minHeap.js
@@ -1,30 +1,31 @@
-export function push(heap, node) {
-  const index = heap.length
-  heap.push(node)
-  siftUp(heap, node, index)
-}
+export const Heap = (heap = [], cmp = (a, b) => a - b) => ({
+  push: (node) => {
+    const index = heap.length;
+    heap.push(node);
+    siftUp(heap, node, index, cmp);
+  },
+  peek: () => (heap.length === 0 ? null : heap[0]),
+  pop: () => {
+    if (heap.length === 0) return null;
 
-export function peek(heap) {
-  return heap.length === 0 ? null : heap[0]
-}
+    const first = heap[0];
+    const last = heap.pop();
+    if (last !== first) {
+      heap[0] = last;
+      siftDown(heap, last, 0, cmp);
+    }
+    return first;
+  },
+  size: () => heap.length,
+  isEmpty: () => heap.length === 0,
+  clear: () => { heap.length = 0; }
+});
 
-export function pop(heap) {
-  if (heap.length === 0) return null
-
-  const first = heap[0]
-  const last = heap.pop()
-  if (last !== first) {
-    heap[0] = last
-    siftDown(heap, last, 0)
-  }
-  return first
-}
-
-function siftUp(heap, node, index) {
+function siftUp(heap, node, index, cmp) {
   while (index > 0) {
     const parentIndex = (index - 1) >>> 1
     const parent = heap[parentIndex]
-    if (compare(parent, node) > 0) {
+    if (cmp(node, parent) < 0) {
       heap[parentIndex] = node
       heap[index] = parent
       index = parentIndex
@@ -32,7 +33,7 @@ function siftUp(heap, node, index) {
   }
 }
 
-function siftDown(heap, node, index) {
+function siftDown(heap, node, index, cmp) {
   const length = heap.length
   const halfLength = length >>> 1
   while (index < halfLength) {
@@ -42,22 +43,17 @@ function siftDown(heap, node, index) {
     const right = heap[rightIndex]
     if (
       rightIndex < length &&
-      compare(right, left) < 0 &&
-      compare(right, node) < 0
+      cmp(right, left) < 0 &&
+      cmp(right, node) < 0
     ) {
       heap[index] = right
       heap[rightIndex] = node
       index = rightIndex
-    } else if (leftIndex < length && compare(left, node) < 0) {
+    } else if (leftIndex < length && cmp(left, node) < 0) {
       heap[index] = left
       heap[leftIndex] = node
       index = leftIndex
     } else return
   }
-}
-
-function compare(a, b) {
-  const diff = a.sortIndex - b.sortIndex
-  return diff !== 0 ? diff : a.id - b.id
 }
 
